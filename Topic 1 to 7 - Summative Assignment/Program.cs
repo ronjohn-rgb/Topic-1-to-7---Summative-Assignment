689ca74101280e7ca7d92d0fb059ad1b2c7f2e53
Aron John Bautista <701035287@GoTVDSB.ca>
2025-03-20 14:36:04 -0400

    Good process

--- Topic 1 to 7 - Summative Assignment/Program.cs
+++ Topic 1 to 7 - Summative Assignment/Program.cs
@@ -4,25 +4,33 @@
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
             int rock, paper, scissor, money, quit;
             bool done = false;
             rock = 1;
             paper = 2;
             scissor = 3;
             quit = 4;
-            money = 100;
+            money = 1000;
             string ready;
             ready = "Yes";
             Console.WriteLine("Rock, Paper, Sciccor!");
             Console.WriteLine("");
             Console.WriteLine("Instructions: Place a bet before playing the game, the game is Rock, Paper, Scissor");
-            Console.WriteLine("To choose from the three options type in the number beside them.");
+            Console.WriteLine("To choose from the four options type in the number beside them.");
             Console.WriteLine("Game starts!");
-            Console.WriteLine("You have 100 dollars!");
-            Console.WriteLine("Money: " + money);
-            int bet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have 1000 dollars!");
+            Console.WriteLine("Money: " + money + "$");         
             Console.WriteLine("Place a bet!");
-            Console.WriteLine("Bet: " + bet);
+            Console.WriteLine("Max bet is 750$");
+            int bet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bet: " + bet + "$");
+            if (bet >= 750)
+            {
+                Console.WriteLine("I said max bet 750$ silly boy!");
+            }
             Console.WriteLine("Play Rock, Paper, Scissor with me! (Type 'Yes' if ready.)");
             Console.WriteLine("Ready?");
             string ready1 = Console.ReadLine();
@@ -30,23 +38,49 @@
             {
                 while (!done)
                 {
+                    money = 100;
                     Random random = new Random();
                     int randNum;
                     int num1 = random.Next(1, 4);
                     Console.WriteLine("Alright!");
                     Console.WriteLine("Choose 1.Rock, 2.Paper, 3.Scissor, 4.Quit.");
-                    int answer1 = Convert.ToInt32(Console.ReadLine());
+                    double answer1 = Convert.ToInt32(Console.ReadLine());
+                   
                     if (answer1 == num1)
                     {
+                        int frequency, length;
+                        frequency = 800;
+                        length = 1500;
+                        Console.Beep(frequency, length);
+                        Console.WriteLine("");
+                        Console.WriteLine("");
                         Console.WriteLine("Nice!");
+                        Console.WriteLine("You won!");
+                        Console.WriteLine("It was " + num1 + "!");
+                        Console.WriteLine("You have " + (money + bet) + "$");
                     }
-                    else
+                    if (answer1 != num1)
                     {
+                        int frequency, length;
+                        frequency = 1500;
+                        length = 1500;
+                        Console.Beep(frequency, length);
+                        Console.WriteLine("");
+                        Console.WriteLine("");
                         Console.WriteLine("Wrong");
-                        done = true;
+                        Console.WriteLine("You lost, try again!");
+                        Console.WriteLine("It was " + num1 + "!");
+                        Console.WriteLine("You have " + (money - bet) + "$ left!");
+                    }   
+                    if (answer1 == 4)
+                    {
+                        Console.WriteLine("Thanks for trying!");
                     }
-
                 }
+                if (money == 0)
+                    {
+                        bool loop = true;
+                    }
             }
             else
             {
